245ff575415cccb231385363559fe3a348cbbc14
hardanimal <hardanimal@hotmail.com>
2024-06-15 10:34:29 +0800

    Update second solution of SQL2 exercise.

--- SQL2.sql
+++ SQL2.sql
@@ -7,3 +7,13 @@ from
 ) tmp
 group by event_type
 having count(*)>1
+
+with deltas as (
+    select distinct event_type,
+    first_value(value) over (partition by event_type order by time desc) -
+    nth_value(value,2) over (partition by event_type order by time desc) as delta
+    from events
+)
+select *
+from deltas
+where delta is not null order by 1
